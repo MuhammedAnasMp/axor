e0cb45b6896bc31f7bcf1fa110edcb37f739bd07
Muhammed Anas <muhammedanasmp2001@gmail.com>
2026-06-19 19:03:14 +0300

    feat: initialize Django project settings with environment-based configuration, CORS support, and database integration

--- backend/axon_backend/settings.py
+++ backend/axon_backend/settings.py
@@ -76,6 +76,7 @@ if not IS_DEVELOPMENT:
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -159,6 +160,15 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
